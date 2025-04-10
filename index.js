9f2e603f265c4007f4c8abc0d14b77a36b57bf59
Anton Kuznetsov <109430794+AntonKuznetsov2022@users.noreply.github.com>
2025-04-10 22:00:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ app.get('/marketdata', async (req, res) => {
   try {
     const response = await axios.get('https://iss.moex.com/iss/engines/stock/markets/shares/boards/TQBR/securities.xml?iss.meta=off&iss.only=marketdata', {
       headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
-        'Accept': '*/*',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36 OPR/117.0.0.0',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
       }
     });
     res.set('Content-Type', 'application/xml');
